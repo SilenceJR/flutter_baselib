1744c6ee6c2b76974c4528606d463a46a30226ae
mocaris <imocaris@outlook.com>
2021-10-09 16:23:07 +0800

    update

--- lib/utils/inputformatter.dart
+++ lib/utils/inputformatter.dart
@@ -3,27 +3,18 @@ import 'package:flutter/services.dart';
 class MoneyInputFormatter extends TextInputFormatter {
   late RegExp _moneyRegExp;
 
-  MoneyInputFormatter({int decimal = 2}) {
-    // _moneyRegExp=RegExp("^([1-9][0-9]*)+(.[0-9]{0,$decimal})?\$");
-    _moneyRegExp = RegExp("^[0-9]+(.[0-9]{0,$decimal})?\$");
-  }
+  MoneyInputFormatter({int decimal = 2}) : _moneyRegExp = RegExp("\^[0-9]+(.[0-9]{0,$decimal})?\$");
 
   @override
   TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
-    var text = newValue.text;
-    if (text.isNotEmpty) {
-      try {
-        if (text.length > 1 && double.parse(text) == 0.0) {
-          text = "0";
-        }
-      } catch (e) {}
-      if (text.startsWith(".")) {
-        text = "0" + text;
-      }
-      if (!_moneyRegExp.hasMatch(text)) {
-        text = oldValue.text;
-      }
+    var oldText = oldValue.text;
+    var newText = newValue.text;
+    if (oldText.isEmpty && newText.startsWith(".")) {
+      newText = "0" + newText;
     }
-    return TextEditingValue(text: text, selection: TextSelection.collapsed(offset: text.length));
+    if (newText.isNotEmpty && !_moneyRegExp.hasMatch(newText)) {
+      newText = oldValue.text;
+    }
+    return TextEditingValue(text: newText, selection: TextSelection.collapsed(offset: newText.length));
   }
 }
