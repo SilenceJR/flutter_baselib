b10b3415b02dffc4a6764825a800633528a4ff1c
mocaris <imocaris@outlook.com>
2021-10-14 17:35:59 +0800

    update

--- lib/utils/image_util.dart
+++ lib/utils/image_util.dart
@@ -16,56 +16,20 @@ class GetImageUtil {
   static GetImageUtil get instance => _instance ??= GetImageUtil();
 
   getUploadImage(ValueChanged<File> valueChanged,
-      {Text? title, int maxSelect = 1, bool crop = false, bool camera = true, int cropWidth = 800, int cropHeight = 800}) {
+      {Text? title, int maxSelect = 1, bool camera = true, int quality = 80, int targetWidth = 0, int targetHeight = 0}) {
     var actions = <CupertinoActionSheetAction>[];
     if (camera) {
       actions.add(CupertinoActionSheetAction(
           onPressed: () async {
             Get.back();
-            try {
-              PermissionUtil.checkPermissionFunc(PhotoManager.requestPermission(), desc: "permission_camera_desc".tr, grantedFunc: () async {
-                AssetEntity? assetEntity = await CameraPicker.pickFromCamera(Get.context!, theme: CameraPicker.themeData(Get.theme.primaryColor));
-                if (null != assetEntity) {
-                  var file = await assetEntity.file;
-                  if (null != file) {
-                    var compressFile = await compressImage(file);
-                    if (null != compressFile) {
-                      valueChanged.call(compressFile);
-                    } else {
-                      valueChanged.call(file);
-                    }
-                  }
-                }
-              });
-            } catch (e) {
-              print("$e");
-            }
+            getImageFromCamera(valueChanged, quality: quality, targetWidth: targetWidth, targetHeight: targetHeight);
           },
           child: Text("camera".tr)));
     }
     actions.add(CupertinoActionSheetAction(
         onPressed: () async {
           Get.back();
-          try {
-            PermissionUtil.checkPermissionFunc(PhotoManager.requestPermission(), desc: "permission_gallery_desc".tr, grantedFunc: () async {
-              List<AssetEntity>? pickAssets = await AssetPicker.pickAssets(Get.context!,
-                  pickerTheme: AssetPicker.themeData(Get.theme.primaryColor), maxAssets: maxSelect, requestType: RequestType.image);
-              if (null != pickAssets && pickAssets.isNotEmpty) {
-                var assetEntity = pickAssets[0];
-                var file = await assetEntity.originFile;
-                if (null != file) {
-                  var compressFile = await compressImage(file);
-                  if (null != compressFile) {
-                    valueChanged.call(compressFile);
-                  } else {
-                    valueChanged.call(file);
-                  }
-                }
-              }
-            });
-          } catch (e) {
-            print("$e");
-          }
+          getImageFromGallery(valueChanged, maxSelect: maxSelect, quality: quality, targetWidth: targetWidth, targetHeight: targetHeight);
         },
         child: Text("gallery".tr)));
     Get.showBottomSheet(CupertinoActionSheet(
@@ -81,8 +45,45 @@ class GetImageUtil {
     ));
   }
 
+  static getImageFromGallery(ValueChanged<File> valueChanged, {int maxSelect = 1, int quality = 80, int targetWidth = 0, int targetHeight = 0}) {
+    try {
+      PermissionUtil.checkPermissionFunc(PhotoManager.requestPermission(), desc: "permission_gallery_desc".tr, grantedFunc: () async {
+        List<AssetEntity>? pickAssets = await AssetPicker.pickAssets(Get.context!,
+            pickerTheme: AssetPicker.themeData(Get.theme.primaryColor), maxAssets: maxSelect, requestType: RequestType.image);
+        if (null != pickAssets && pickAssets.isNotEmpty) {
+          var assetEntity = pickAssets[0];
+          var file = await assetEntity.originFile;
+          if (null != file) {
+            var compressFile = await compressImage(file, quality: quality, targetWidth: targetWidth, targetHeight: targetHeight);
+            valueChanged.call(compressFile ?? file);
+          }
+        }
+      });
+    } catch (e) {
+      print("$e");
+    }
+  }
+
+  static getImageFromCamera(ValueChanged<File> valueChanged, {int quality = 80, int targetWidth = 0, int targetHeight = 0}) {
+    try {
+      PermissionUtil.checkPermissionFunc(PhotoManager.requestPermission(), desc: "permission_camera_desc".tr, grantedFunc: () async {
+        AssetEntity? assetEntity = await CameraPicker.pickFromCamera(Get.context!, theme: CameraPicker.themeData(Get.theme.primaryColor));
+        if (null != assetEntity) {
+          var file = await assetEntity.file;
+          if (null != file) {
+            var compressFile = await compressImage(file, quality: quality, targetHeight: targetHeight, targetWidth: targetWidth);
+            valueChanged.call(compressFile ?? file);
+          }
+        }
+      });
+    } catch (e) {
+      print("$e");
+    }
+  }
+
   static Future<File?> compressImage(File imageFile, {int quality = 80, int targetWidth = 0, int targetHeight = 0}) async {
-    File compressImage = await FlutterNativeImage.compressImage(imageFile.path, quality: quality, targetWidth: targetWidth, targetHeight: targetHeight);
+    File compressImage =
+        await FlutterNativeImage.compressImage(imageFile.path, quality: quality, targetWidth: targetWidth, targetHeight: targetHeight);
     return compressImage;
   }
 
