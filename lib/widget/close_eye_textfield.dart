4b8638755cf6e714a2829a3d28ad336b918b133c
mocaris <imocaris@outlook.com>
2021-11-24 19:40:02 +0800

    输入框垂直居中

--- lib/widget/close_eye_textfield.dart
+++ lib/widget/close_eye_textfield.dart
@@ -13,7 +13,7 @@ class CloseEyeTextField extends StatefulWidget {
   final Widget? suffixIcon;
   final Widget? prefixIcon;
   final bool? obscureText;
-  final TextEditingController controller;
+  final TextEditingController? controller;
   final List<TextInputFormatter>? inputFormatters;
   final Decoration? decoration;
   final Iterable<String>? autofillHints;
@@ -32,34 +32,40 @@ class CloseEyeTextField extends StatefulWidget {
   final TextAlign? textAlign;
   final bool autofocus;
   final bool readOnly;
+  final double? cursorWidth;
+  final Radius? cursorRadius;
+  final bool centerVertical;
 
   CloseEyeTextField(
       {this.hint,
-      this.showClearIcon = true,
-      this.showHideIcon = false,
-      this.readOnly = false,
-      this.prefixIcon,
-      this.suffixIcon,
-      //  隐藏 文字 以 密码形式显示
-      this.obscureText,
-      required this.controller,
-      this.inputFormatters,
-      this.decoration,
-      this.autofillHints,
-      this.textInputAction,
-      this.keyboardType,
-      this.onSubmitted,
-      this.onChanged,
-      this.decorationPadding,
-      this.decorationMargin,
-      this.focusNode,
-      this.textFieldHeight,
-      this.style,
-      this.hintStyle,
-      this.textAlign,
-      this.autofocus = false,
-      this.textBaseline,
-      this.crossAxisAlignment = CrossAxisAlignment.center});
+        this.showClearIcon = true,
+        this.showHideIcon = false,
+        this.readOnly = false,
+        this.prefixIcon,
+        this.suffixIcon,
+        //  隐藏 文字 以 密码形式显示
+        this.obscureText,
+        this.controller,
+        this.inputFormatters,
+        this.decoration,
+        this.autofillHints,
+        this.textInputAction,
+        this.keyboardType,
+        this.onSubmitted,
+        this.onChanged,
+        this.decorationPadding,
+        this.decorationMargin,
+        this.focusNode,
+        this.textFieldHeight,
+        this.style,
+        this.hintStyle,
+        this.textAlign,
+        this.autofocus = false,
+        this.textBaseline,
+        this.cursorWidth,
+        this.cursorRadius,
+        this.centerVertical = true,
+        this.crossAxisAlignment = CrossAxisAlignment.center});
 
   @override
   State<StatefulWidget> createState() => _State();
@@ -69,10 +75,13 @@ class _State extends State<CloseEyeTextField> {
   bool _obscureText = false;
   late FocusNode _focusNode;
   bool _hasFocus = false;
+  double? _height;
+  late TextEditingController _controller;
 
   @override
   void initState() {
     super.initState();
+    _controller = widget.controller ?? TextEditingController();
     _obscureText = widget.obscureText ?? (widget.showHideIcon && null == widget.suffixIcon);
     _focusNode = widget.focusNode ?? FocusNode();
     _focusNode.addListener(() {
@@ -80,49 +89,57 @@ class _State extends State<CloseEyeTextField> {
         _hasFocus = _focusNode.hasFocus;
       });
     });
+    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+      _height ??= (widget.textFieldHeight ?? context.size?.height);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     var _style = widget.style ?? Theme.of(context).textTheme.bodyText2;
+    var textPainter =
+    TextPainter(text: TextSpan(text: '', style: _style), textDirection: TextDirection.ltr, textWidthBasis: TextWidthBasis.longestLine)
+      ..layout();
     return Container(
       height: widget.textFieldHeight,
       decoration: widget.decoration,
       alignment: Alignment.center,
       margin: widget.decorationMargin,
       padding: widget.decorationPadding,
+      color: Colors.blue.withOpacity(0.3),
       child: Row(
         textBaseline: widget.textBaseline,
         crossAxisAlignment: widget.crossAxisAlignment,
         children: [
           Visibility(visible: null != widget.prefixIcon, child: Container(child: widget.prefixIcon)),
           Expanded(
-            child: TextField(
-              style: _style,
-              controller: widget.controller,
-              maxLines: 1,
-              autofocus: widget.autofocus,
-              cursorRadius: Radius.circular(2),
-              cursorWidth: 1.5,
-              readOnly: widget.readOnly,
-              focusNode: _focusNode,
-              textAlignVertical: TextAlignVertical.top,
-              keyboardType: widget.keyboardType,
-              autofillHints: widget.autofillHints,
-              onSubmitted: widget.onSubmitted,
-              onChanged: widget.onChanged,
-              textAlign: widget.textAlign ?? TextAlign.start,
-              textInputAction: widget.textInputAction ?? TextInputAction.done,
-              obscureText: _obscureText,
-              inputFormatters: widget.inputFormatters,
-              decoration: InputDecoration(
-                  hintStyle: widget.hintStyle ??
-                      (Theme.of(context).inputDecorationTheme.hintStyle ?? _style)?.copyWith(color: Theme.of(context).hintColor),
-                  hintText: widget.hint,
-                  isDense: true,
-                  border: InputBorder.none),
-            ),
-          ),
+              child: TextField(
+                  style: _style,
+                  controller: _controller,
+                  maxLines: 1,
+                  autofocus: widget.autofocus,
+                  cursorRadius: widget.cursorRadius ?? Radius.circular(1.5),
+                  cursorWidth: widget.cursorWidth ?? 1.5,
+                  readOnly: widget.readOnly,
+                  focusNode: _focusNode,
+                  textAlignVertical: TextAlignVertical.top,
+                  keyboardType: widget.keyboardType,
+                  autofillHints: widget.autofillHints,
+                  onSubmitted: widget.onSubmitted,
+                  onChanged: widget.onChanged,
+                  textAlign: widget.textAlign ?? TextAlign.start,
+                  textInputAction: widget.textInputAction ?? TextInputAction.done,
+                  obscureText: _obscureText,
+                  inputFormatters: widget.inputFormatters,
+                  decoration: InputDecoration(
+                      hintStyle: widget.hintStyle ??
+                          (Theme.of(context).inputDecorationTheme.hintStyle ?? _style)?.copyWith(color: Theme.of(context).hintColor),
+                      hintText: widget.hint,
+                      isDense: true,
+                      contentPadding: (null != _height && widget.centerVertical)
+                          ? EdgeInsets.symmetric(vertical: (_height! - textPainter.height) / 2)
+                          : null,
+                      border: InputBorder.none))),
           Visibility(visible: _hasFocus, child: _clearIcon()),
           Visibility(visible: widget.showHideIcon, child: widget.suffixIcon ?? _hideSuffixIcon(context))
         ],
@@ -132,23 +149,17 @@ class _State extends State<CloseEyeTextField> {
 
   _clearIcon() {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 5),
-      child: GestureDetector(
-        onTap: () => widget.controller.clear(),
-        child: Icon(Icons.cancel, size: 20, color: Colors.grey),
-      ),
-    );
+      // margin: EdgeInsets.symmetric(horizontal: 5),
+        child: GestureDetector(onTap: () => _controller.clear(), child: Icon(Icons.cancel, size: 20, color: Colors.grey)));
   }
 
   _hideSuffixIcon(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 5),
-      child: GestureDetector(
-        onTap: () => setState(() {
-          _obscureText = !_obscureText;
-        }),
-        child: Icon(_obscureText ? CupertinoIcons.eye : CupertinoIcons.eye_slash, color: Colors.grey, size: 20),
-      ),
-    );
+        margin: EdgeInsets.symmetric(horizontal: 5),
+        child: GestureDetector(
+            onTap: () => setState(() {
+              _obscureText = !_obscureText;
+            }),
+            child: Icon(_obscureText ? CupertinoIcons.eye : CupertinoIcons.eye_slash, color: Colors.grey, size: 20)));
   }
 }
