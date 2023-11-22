7810bb1aabdc953b99c5293b9b8254901bd05bee
Silence <pjhkd1@hotmail.com>
2023-11-23 00:37:24 +0800

    debug logger

--- lib/utils/log_util.dart
+++ lib/utils/log_util.dart
@@ -1,12 +1,15 @@
 import 'dart:developer' as d;
 
+import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart';
 
 Logger logger = Logger(filter: AppLogFilter(), output: AppLogOutput());
 
 class AppLogFilter extends LogFilter {
   @override
-  bool shouldLog(LogEvent event) => event.level == Level.error || (event.level.index >= level!.index);
+  bool shouldLog(LogEvent event) =>
+      event.level == Level.error ||
+      (!kReleaseMode && event.level.index >= level!.index);
 }
 
 class AppLogOutput extends LogOutput {
