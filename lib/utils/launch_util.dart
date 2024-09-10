279e25e611ecdacb9d8f1bf492827ee0e1f6f534
silence <pengjian@toro-tech.com>
2024-09-10 09:16:16 +0800

    update version

--- lib/utils/launch_util.dart
+++ lib/utils/launch_util.dart
@@ -12,4 +12,20 @@ class LaunchUtil {
   static Future<bool> sendMail(String mailAddress) {
     return launchUrlString("mailto:$mailAddress");
   }
+
+  static Future<bool> openUrl(String url) {
+    return launchUrlString(url);
+  }
+
+  static Future<bool> openMap(String address) {
+    return launchUrlString("geo:0,0?q=$address");
+  }
+
+  static Future<bool> openMapWithLatLng(double lat, double lng) {
+    return launchUrlString("geo:$lat,$lng");
+  }
+
+  static Future<bool> checkUrl(String url) {
+    return canLaunchUrlString(url);
+  }
 }
